a31ee069695dba50b7e48ae27a752faf2a60199c
thomasdelhomenie <thomas.delhomenie@gmail.com>
2011-11-02 22:09:39 +0100

    Debut de l'implementation de l'écran d'options

---
+++ src/fr/pronoschallenge/options/OptionsActivity.java
@@ -0,0 +1,12 @@
+package fr.pronoschallenge.options;
+
+import greendroid.app.GDTabActivity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Thomas Delhoménie
+ * Date: 02/11/11
+ * Time: 22:01
+ */
+public class OptionsActivity extends GDTabActivity {
+}
